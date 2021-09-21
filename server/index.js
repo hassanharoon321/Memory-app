5cad89ea448361cf305915d344289482c8102248
hassanhaoon321 <hassanharoon321@gmail.com>
2021-09-21 09:03:36 +0500

    aded

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import productRoutes from "./routes/Posts.js"
+
+const app = express();
+
+app.use("/posts", productRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://admin:admin@cluster0.5uiag.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+    .catch((err) => console.log(err.message))
+
+// mongoose.set("useFindAndModify", false);
