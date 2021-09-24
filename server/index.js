f9ad755efe008b61a626b193da80589c5faffaae
hassanhaoon321 <hassanharoon321@gmail.com>
2021-09-24 21:55:29 +0500

    aded

--- server/index.js
+++ server/index.js
@@ -7,11 +7,12 @@ import productRoutes from "./routes/Posts.js"
 
 const app = express();
 
-app.use("/posts", productRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use("/posts", productRoutes);
 
 const CONNECTION_URL = "mongodb+srv://admin:admin@cluster0.5uiag.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
